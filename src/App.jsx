ab2c142515f2129f1db22179969179bea08c23a1
pranamj <109278909+pranamj@users.noreply.github.com>
2023-11-06 07:28:44 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+            import './App.css';
+
+            function App() {
+              return (
+                <main>
+                  <h1>Programming Multiverse</h1>
+                  <div className='box'>
+
+                    <div className='card-row'>
+                      <div className='card'>
+                        <h2>Java</h2>
+                        <p>
+                          Year of Start: 1995<br />
+                          Creator: James Gosling<br />
+                          Use Case: Enterprise software, Android apps
+                        </p>
+                      </div>
+
+                      <div className='card'>
+                        <h2>JavaScript</h2>
+                        <p>
+                          Year of Start: 1995<br />
+                          Creator: Brendan Eich<br />
+                          Use Case: Web development, browser scripting
+                        </p>
+                      </div>
+
+                      <div className='card'>
+                        <h2>PHP</h2>
+                        <p>
+                          Year of Start: 1995<br />
+                          Creator: Rasmus Lerdorf<br />
+                          Use Case: Web development, server-side scripting
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className='card-row'>
+                      <div className='card'>
+                        <h2>C#</h2>
+                        <p>
+                          Year of Start: 2000<br />
+                          Creator: Microsoft<br />
+                          Use Case: Windows apps, game development
+                        </p>
+                      </div>
+
+                      <div className='card'>
+                        <h2>Scala</h2>
+                        <p>
+                          Year of Start: 2003<br />
+                          Creator: Martin Odersky<br />
+                          Use Case: Web applications, data analysis
+                        </p>
+                      </div>
+
+                      <div className='card'>
+                        <h2>Golang</h2>
+                        <p>
+                          Year of Start: 2009<br />
+                          Creator: Robert Griesemer, Rob Pike, Ken Thompson<br />
+                          Use Case: Cloud computing, microservices
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className='card-row'>
+                      <div className='card'>
+                        <h2>Rust</h2>
+                        <p>
+                          Year of Start: 2010<br />
+                          Creator: Graydon Hoare<br />
+                          Use Case: Systems programming, safety
+                        </p>
+                      </div>
+
+                      <div className='card'>
+                        <h2>Kotlin</h2>
+                        <p>
+                          Year of Start: 2011<br />
+                          Creator: JetBrains<br />
+                          Use Case: Android app development, server-side
+                        </p>
+                      </div>
+
+                      <div className='card'>
+                        <h2>Swift</h2>
+                        <p>
+                          Year of Start: 2014<br />
+                          Creator: Apple Inc.<br />
+                          Use Case: iOS, macOS app development
+                        </p>
+                      </div>
+                    </div>
+
+                  </div>
+                </main>
+              );
+            }
+
+            export default App;
